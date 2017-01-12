6c7731a508bbd9da2d4167fd77aeaa1b13fe84b8
Travis Frisinger <tmfrisinger@gmail.com>
2017-01-12 21:52:41 +0400

    Interace cleanup for ISpeedySqlLocalDb

--- source/TddBuddy.SpeedySqlLocalDb/ISpeedySqlLocalDb.cs
+++ source/TddBuddy.SpeedySqlLocalDb/ISpeedySqlLocalDb.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Data.Entity;
-using System.Data.SqlClient;
-
-namespace TddBuddy.SpeedySqlLocalDb
+﻿namespace TddBuddy.SpeedySqlLocalDb
 {
     public interface ISpeedySqlLocalDb
     {
-        //void BootstrapDatabaseForEfMigrations(Func<SqlConnection, DbContext> createDbContextFunc);
         ISpeedySqlLocalDbWrapper CreateSpeedyLocalDbWrapper();
-        //void DetachDatabase();
     }
 }
