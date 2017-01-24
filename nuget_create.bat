8207353fb1cf9b6b2387a39e0842a5c6264ce2ed
Travis Frisinger <tmfrisinger@gmail.com>
2017-01-24 21:46:25 +0400

    Updated nuget package script

--- nuget_create.bat
+++ nuget_create.bat
@@ -2,3 +2,4 @@ cd C:\Systems\SpeedyLocalDb\source\TddBuddy.SpeedySqlLocalDb
 REM C:\Apps\nuget.exe spec
 REM EDIT spec file
 C:\Apps\nuget.exe pack TddBuddy.SpeedySqlLocalDb.csproj
+move C:\Systems\SpeedyLocalDb\source\TddBuddy.SpeedySqlLocalDb\*.nupkg C:\Systems\SpeedyLocalDb
