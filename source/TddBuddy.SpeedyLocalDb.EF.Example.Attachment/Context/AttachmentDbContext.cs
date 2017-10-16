ba8fbeca63323e6e48cb3b6e1f1fa4987a1e26bb
T-rav <tmfrisinger@gmail.com>
2017-10-16 18:42:07 +0200

    Remove comment

--- source/TddBuddy.SpeedyLocalDb.EF.Example.Attachment/Context/AttachmentDbContext.cs
+++ source/TddBuddy.SpeedyLocalDb.EF.Example.Attachment/Context/AttachmentDbContext.cs
@@ -2,17 +2,15 @@ using System.Data.Common;
 using System.Data.Entity;
 
 namespace TddBuddy.SpeedyLocalDb.EF.Example.Attachment.Context
-{ 
-
+{
     public class AttachmentDbContext : DbContext
     {
-        // NOTE: This would be a production contructor, here for illustration 
+        public AttachmentDbContext(DbConnection connection) : base(connection, false){ }
+
         public AttachmentDbContext() : base("AttachmentContext")
         {
             Database.SetInitializer<AttachmentDbContext>(null);
         }
-
-        public AttachmentDbContext(DbConnection connection) : base(connection, false){ }
 
         public IDbSet<Entities.Attachment> Attachments { get; set; }
 
