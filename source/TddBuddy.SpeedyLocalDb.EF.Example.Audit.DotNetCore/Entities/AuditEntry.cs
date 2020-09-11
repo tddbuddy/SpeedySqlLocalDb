86bd58aaa82e296ba30f194a5906d3c14a51a4b3
Trav <t@koderex.co.za>
2020-09-11 14:29:19 -0600

    chore: package upgrades to 3.1

--- source/TddBuddy.SpeedyLocalDb.EF.Example.Audit.DotNetCore/Entities/AuditEntry.cs
+++ source/TddBuddy.SpeedyLocalDb.EF.Example.Audit.DotNetCore/Entities/AuditEntry.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TddBuddy.SpeedyLocalDb.EF.Example.Audit.DotNetCore.Entities
 {
+    [Table("Entries", Schema = "aud")]
     public class AuditEntry
     {
         [Key]
